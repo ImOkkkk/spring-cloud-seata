512806401eb756c4400dfaf88986e9df28cf51f9
ImOkkkk <547717253@qq.com>
2023-01-06 21:43:08 +0800

    feat: 整合nacos配置中心；sentinel整合nacos

--- user/user-service/src/main/java/com/imokkkk/user/service/impl/UserServiceImpl.java
+++ user/user-service/src/main/java/com/imokkkk/user/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.imokkkk.user.service.impl;
 
 import com.imokkkk.user.pojo.entity.User;
 import com.imokkkk.user.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,12 +12,14 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserServiceImpl implements UserService {
+    @Value("${user.name:OJ}")
+    private String name;
 
     @Override
     public User getByUserId(String userId) {
         User user = new User();
         user.setUserId(userId);
-        user.setName("张三");
+        user.setName(name);
         return user;
     }
 }
